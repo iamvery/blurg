040f1dcfd02b4bc42af4de84fc0220ab47eb07c2
Jay Hayes <ur@iamvery.com>
2016-09-23 08:11:09 -0500

    Add Post model with title and body

---
+++ priv/repo/migrations/20160923050839_create_post.exs
@@ -0,0 +1,13 @@
+defmodule Blurg.Repo.Migrations.CreatePost do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :title, :string, null: false
+      add :body, :text, null: false
+
+      timestamps()
+    end
+
+  end
+end
