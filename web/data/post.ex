3cb7108bda3c09d92ca9fca71fe3603287bd7c4f
Jay Hayes <ur@iamvery.com>
2016-09-23 08:53:45 -0500

    Include post index links

--- web/data/post.ex
+++ web/data/post.ex
@@ -1,7 +1,14 @@
 defmodule Blurg.Data.Post do
   use Blurg.Data
+  import Blurg.Router.Helpers
 
-  def build(_conn, :index, _opts) do
-    Blurg.Repo.all(Blurg.Post)
+  def build(conn, :index, _opts) do
+    Enum.map Blurg.Repo.all(Blurg.Post), fn post ->
+      %{
+        title: post.title,
+        show_link: [href: post_path(conn, :show, post)],
+        edit_link: [href: post_path(conn, :edit, post)],
+      }
+    end
   end
 end
