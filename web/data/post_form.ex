767cf0ed83270a80e649704da9da3a82109f438b
Jay Hayes <ur@iamvery.com>
2016-09-28 16:24:41 -0500

    Wire up post updating

--- web/data/post_form.ex
+++ web/data/post_form.ex
@@ -12,4 +12,16 @@ defmodule Blurg.Data.PostForm do
       action: post_path(conn, :create), method: "post",
     }
   end
+
+  def build(%{params: %{"id" => id}} = conn, action, [post, changeset]) when action in [:edit, :update] do
+    {
+      %{
+        csrf: [name: "_csrf_token", value: Phoenix.Controller.get_csrf_token],
+        method: [name: "_method", value: "patch"],
+        title: [name: "post[title]", value: Map.get(changeset.changes, :title, post.title)],
+        body: {Map.get(changeset.changes, :body, post.body), name: "post[body]"},
+      },
+      action: post_path(conn, :update, id), method: "post",
+    }
+  end
 end
